ac648392f741efe288caa2c1f51bbe36a4b65ab6
SteveMcStevenson <42510918+SteveMcStevenson@users.noreply.github.com>
2023-11-08 05:44:28 +0800

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,10 @@ void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.deepPurple,
+        backgroundColor: Colors.lightGreen,
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.deepPurple,
+          backgroundColor: Colors.lightGreen,
         ),
         body: DicePage(),
       ),
@@ -24,49 +24,71 @@ class DicePage extends StatefulWidget {
 }
 
 class _DicePageState extends State<DicePage> {
-  int left = 1, right = 1;
+  int left = 1, right = 1, d20 = 1;
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
+    return Column(
+      children: [
+        Center(
+          child: Row(
+                children: [
+                  Expanded(child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextButton(
+                        onPressed: (){
+                        setState(() {
+                          ChangeValueD6();
+                        });
+                        },
+                        child: Image.asset('images/dice$left.png')
+                    ),
+                  )),
+                  Expanded(child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextButton(
+                        onPressed: (){
+                          setState(() {
+                            ChangeValueD6();
+
+                          });
+                        },
+                        child: Image.asset('images/dice$right.png')),
+                  )),
+
+                ],
+              ),
+
+          ),
+        Center(
+          child: Row(
             children: [
               Expanded(child: Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: TextButton(
                     onPressed: (){
-                    setState(() {
-                      ChangeValue();
-                    });
-                    },
-                    child: Image.asset('images/dice$left.png')
-                ),
-              )),
-              Expanded(child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextButton(
-                    onPressed: (){
                       setState(() {
-                        ChangeValue();
-
+                        ChangeValueD20();
                       });
                     },
-                    child: Image.asset('images/dice$right.png')),
-              )),
-
+                    child: Image.asset('images/d20_$d20.png')
+                ),
+              )
+              ),
             ],
           ),
 
-      );
+        )
+      ],
+    );
 
   }
 
-  void ChangeValue(){
+  void ChangeValueD6(){
     left = 1 + Random().nextInt(6);
     right = 1 + Random().nextInt(6);
   }
-
-  void RollSum(){
-    'The sum of the dice is: '; left+right;
-  }
+ void ChangeValueD20(){
+    d20 = 1 + Random().nextInt(20);
+ }
 }
 
